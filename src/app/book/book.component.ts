66492df2502c2496d1208e6e61861a16f0536b51
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-11-14 21:25:55 +0000

    Completed First Angular Sheet

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import {Book} from '../book';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  books: Book[] = [];
+  message!: string;
+
+  constructor(private bookService : BookService) { }
+
 
   ngOnInit(): void {
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.books = value,
+      complete: () => console.log('book service finished'),
+      error: (message) => this.message = message
+
+    }) 
+
   }
 
 }
